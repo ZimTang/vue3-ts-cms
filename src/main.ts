6c59a39efaa5f98c8fa7f34fc171d5f64716a3dd
twitzz <2351061127@qq.com>
2022-01-26 15:22:57 +0800

    fix: element-plus部分样式丢失

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import twRequest from './request'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
